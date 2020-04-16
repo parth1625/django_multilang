1098b4f727de0dd90364c577f0c145baf736f19b
Parth Vijay <parthvj1998@gmail.com>
2020-04-16 18:22:49 +0530

    new model created

--- home/models.py
+++ home/models.py
@@ -11,3 +11,6 @@ LANGUAGE_CHOICES = (
 class User(AbstractUser):
 	is_trailing = models.BooleanField(default=True)
 	language = models.CharField(default='en-us', choices=LANGUAGE_CHOICES, max_length=5)
+
+class LangTranslation(models.Model):
+	language = models.CharField(default='en-us', choices=LANGUAGE_CHOICES, max_length=5)
